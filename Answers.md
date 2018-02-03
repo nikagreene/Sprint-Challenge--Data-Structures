946bcbf2b99678cfc443ad3a472d13c97d473742
Shawniqua Greene <shawny.b12@gmail.com>
2018-02-03 15:00:35 -0500

    Shawniqua Greene: Sprint Challenge data structures

---
+++ Answers.md
@@ -0,0 +1,26 @@
+1. What are the order of insertions/removals for the following data structures?
+   - **Stack**
+
+   The order of insertions/removals for stacks is LIFO or last-in-first-out. The items near/closest to the base are the items that have been there the longest. Items are added and removed from the top. 
+
+   - **Queue**
+
+   The order of insertions/removals for stacks is FIFO or first-in-first-out.
+   The items near/closest to the front are the items that have been there the longest. Items are added at the end of the queue, while removal takes place at the front.
+
+2. What is the retreival time complexity for the following data structures?
+   - **Linked List**
+
+   For accessing and searching a linked list, the time complexity is O(n). Which Means it is linear and in proportion to the size of the data set. For inserting and deleting time complexity is O(1). Which means it is constant time and will always execute at the same time regardless of the size of data. 
+
+   - **Hash Table**
+
+    In most cases, Hash Tables time complexity is O(1). 
+
+   - **Binary Search Trees**
+
+    For searching, accesssing, inserting and deleting, the time complexity for Binary Search Tree is O(log n). 
+
+3. What are some advantages to using a Hash Tables over an array in JavaScript?
+
+    The time complexity for a hash table is faster than that of an array. 
--- src/hash-table-helpers.js
+++ src/hash-table-helpers.js
@@ -34,6 +34,60 @@ class LimitedArray {
     this.storage[index] = value;
   }
 }
+
+/* eslint-disable class-methods-use-this */
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    // Do not modify anything inside of the constructor
+  }
+  // Wraps the given value in a node object and adds the node to the tail of the list
+  // If the list is empty, the new element is considered the tail as well as the head
+  // If there is one element in the list before the new element is added, the new element becomes the tail of the list
+  addToTail(value) {
+    const node = {
+      next: null,
+      value,
+    };
+
+    if (this.tail === null) {
+      this.tail = node;
+      this.head = node;
+      return;
+    }
+    this.tail.next = node;
+    this.tail = node;
+  }
+  // Removes the current head node from the list, replacing it with the next element in the list
+  // Returns the value of the removed node
+  removeHead() {
+    if (this.head === null) {
+      return;
+    }
+    const value = this.head.value;
+    this.head = this.head.next;
+    return value;
+  }
+  // Checks the linked list for the given value
+  // Returns true if the the value is found in the list, false otherwise
+  contains(value) {
+    if (this.head === null) {
+      return false;
+    }
+    const find = (node) => {
+      if (node.value === value) {
+        return true;
+      }
+      if (node.next === null) {
+        return false;
+      }
+      return (find(node.next));
+    };
+    return (find(this.head));
+  }
+}
 /* eslint-disable no-bitwise, operator-assignment */
 // This is hash function you'll be using to hash keys
 // There's some bit-shifting magic going on here, but essentially, all it is doing is performing the modulo operator
@@ -52,4 +106,5 @@ const getIndexBelowMax = (str, max) => {
 module.exports = {
   LimitedArray,
   getIndexBelowMax,
+  LinkedList,
 };
